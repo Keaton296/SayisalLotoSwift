b6e8c95ebc7dfa021cdfbfcd8a459fb0ccaf892b
Kaan Dalkıran <kaan51dlkrn@gmail.com>
2024-06-24 12:33:25 +0300

    Initial Commit

---
+++ SayisalLoto/SayisalLotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayisalLotoApp.swift
+//  SayisalLoto
+//
+//  Created by Trakya11 on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SayisalLotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
